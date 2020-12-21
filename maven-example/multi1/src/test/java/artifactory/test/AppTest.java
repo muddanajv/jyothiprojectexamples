b10bea423615586c3e4edd11fb4ba3d03d59ed91
muddanajv@jfrog.com <muddanajv@jfrog.com>
2020-12-21 17:54:49 +0530

    test commit

--- maven-example/multi1/src/test/java/artifactory/test/AppTest.java
+++ maven-example/multi1/src/test/java/artifactory/test/AppTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * jyothi testUnit test for simple App.
  */
 public class AppTest 
     extends TestCase
